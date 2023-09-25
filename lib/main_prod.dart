3723e4f7febf949fb7329936196c25250cc43ad0
Brian Mutinda <mutindabrian26@gmail.com>
2023-09-25 16:08:34 +0300

    add prod file

---
+++ lib/main_prod.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flavor/app_config.dart';
+
+import 'main.dart';
+
+void main() async {
+  AppConfig.create(
+    appName: "Prod Flavor App",
+    baseUrl: "https://api.prod.com",
+    primaryColor: Colors.blue,
+    flavor: Flavor.prod,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppConfig.shared.appName,
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: AppConfig.shared.primaryColor),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
